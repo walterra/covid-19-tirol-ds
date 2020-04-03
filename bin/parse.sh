5c9e02cb6d0c308ee8ebe79972799c42d2035fca
Christian Junker <chjdev@users.noreply.github.com>
2020-04-03 16:08:19 +0200

    fix crawl scripts: infer directoryname, remove eval, better argument handling

--- bin/parse.sh
+++ bin/parse.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
-node ./src/parse_trauerhilfe scrape_trauerhilfe_20200327_174811
-node ./src/parse_kuratorium_bestattung scrape_kuratorium_bestattung_20200327_180153
-node ./src/parse_dellemann scrape_dellemann
-node ./src/parse_kroell scrape_kroell
+dir=`dirname $0`
+
+cd $dir/../scrape
+trauerhilfe=`ls -d1 */ | grep trauerhilfe | sed "s/\///"`
+kuratorium=`ls -d1 */ | grep kuratorium | sed "s/\///"`
+dellemann=`ls -d1 */ | grep dellemann | sed "s/\///"`
+kroell=`ls -d1 */ | grep kroell | sed "s/\///"`
+cd -
+
+cd $dir/..
+echo "parsing trauerhilfe"
+for s in $trauerhilfe
+do
+  node ./src/parse_trauerhilfe $s
+done
+
+echo "parsing kuratorium"
+
+for s in $kuratorium
+do
+  node ./src/parse_kuratorium_bestattung $s
+done
+
+echo "parsing dellemann"
+for s in $dellemann
+do
+  node ./src/parse_dellemann $s
+done
+
+echo "parsing kroell"
+for s in $kroell
+do
+  node ./src/parse_kroell $s
+done
+
+echo "running deduplication"
 node ./src/parse_deduplicate
+cd -
--- src/parse_deduplicate.js
+++ src/parse_deduplicate.js
@@ -1,23 +1,9 @@
 // cheerio is a jquery API inspired lib for nodejs
 const cheerio = require('cheerio')
 const fs = require('fs');
-
-// setup csv extension so we can simply require it
-require.extensions['.csv'] = function (module, filename) {
-  module.exports = fs.readFileSync(filename, 'utf8');
-};
-
-// load CSV and split lines to array
-const rows1 = require(`../data/scrape_kuratorium_bestattung_20200327_180153.csv`).split('\n');
-const rows2 = require(`../data/scrape_trauerhilfe_20200327_174811.csv`).split('\n');
-const rows3 = require(`../data/scrape_dellemann.csv`).split('\n');
-const rows4 = require(`../data/scrape_kroell.csv`).split('\n');
-
-const rows = [];
-rows.push(...rows1);
-rows.push(...rows2);
-rows.push(...rows3);
-rows.push(...rows4);
+const glob = require("glob")
+ 
+const rows = glob.sync("./data/scrape*.csv").flatMap((filename) => fs.readFileSync(filename, 'utf8').split('\n'));
 
 console.log(`Running deduplication ...`);
 console.log(`rows: ${rows.length}`);
